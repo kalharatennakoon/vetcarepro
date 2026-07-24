d55e87824b40bb30c490c38a91ecc6338c6df669
kalharatennakoon <kalharatennakoonmck@gmail.com>
2026-07-24 19:20:26 +0530

    feat: initialize VetCare project with essential files, configurations, and initial SwiftUI views feat: add project structure including app icon and accent color assets feat: implement basic unit and UI tests for the application chore: add workspace and user interface state files for project management chore: create xcschememanagement.plist for scheme management chore: add AccentColor and AppIcon asset catalogs

---
+++ mobile/ios/VetCare/VetCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VetCare
+//
+//  Created by Kalhara Tennakoon on 2026-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile/ios/VetCare/VetCare/VetCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  VetCareApp.swift
+//  VetCare
+//
+//  Created by Kalhara Tennakoon on 2026-07-24.
+//
+
+import SwiftUI
+
+@main
+struct VetCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
