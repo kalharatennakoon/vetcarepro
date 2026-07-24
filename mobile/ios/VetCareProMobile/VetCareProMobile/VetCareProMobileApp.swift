da1e14fcff4f384b4e3473c8ddc74e30acf70bbe
kalharatennakoon <kalharatennakoonmck@gmail.com>
2026-07-24 19:12:49 +0530

    Initial commit of VetCareProMobile project with essential files and configurations. Added project structure including Xcode project files, workspace settings, assets for app icon and accent color, and initial SwiftUI views. Implemented basic unit and UI tests for the application.

---
+++ mobile/ios/VetCareProMobile/VetCareProMobile/VetCareProMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  VetCareProMobileApp.swift
+//  VetCareProMobile
+//
+//  Created by Kalhara Tennakoon on 2026-07-24.
+//
+
+import SwiftUI
+
+@main
+struct VetCareProMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobile/ios/VetCareProMobile/VetCareProMobileTests/VetCareProMobileTests.swift
@@ -0,0 +1,19 @@
+//
+//  VetCareProMobileTests.swift
+//  VetCareProMobileTests
+//
+//  Created by Kalhara Tennakoon on 2026-07-24.
+//
+
+import Testing
+@testable import VetCareProMobile
+
+struct VetCareProMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
